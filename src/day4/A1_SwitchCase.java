2f4348fb8efdd4a369f9f3d91ce6c531e74a98ed
Hanamantgit7259 <hanamant71198@gmail.com>
2025-08-14 08:11:43 +0530

    New Programm Added

---
+++ src/day4/A1_SwitchCase.java
@@ -0,0 +1,36 @@
+package day4;
+
+import java.util.Scanner;
+
+public class A1_SwitchCase {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Select the Food that you wnat to order :");
+		String food = sc.nextLine();
+		
+
+		switch (food) {
+
+		case "Idly": {
+			System.out.println("Customer wants Idly");
+			break;
+		}
+		case "Upma": {
+			System.out.println("Customer wants Upma");
+			break;
+		}
+		case "Dosa": {
+			System.out.println("Customer wants dosa");
+			break;
+		}
+		default: {
+
+			System.out.println("Sever default Vada");
+
+		}
+
+		}
+	}
+}
